a99952cc43810aba979f453134fd7727e7eb44d1
@serufu_yua <serufu_yua@nya.com>
2026-05-05 09:50:32 +0500

    update test ui

--- demo/list-box-test/code/gameinitialize.pas
+++ demo/list-box-test/code/gameinitialize.pas
@@ -50,7 +50,7 @@ initialization
 
   Window := TCastleWindow.Create(Application);
   Window.Width:= 800;
-  Window.Height:= 900;
+  Window.Height:= 800;
   Application.MainWindow := Window;
 
   { Optionally, adjust window fullscreen state and size at this point.
